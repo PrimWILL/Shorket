f23b547a5e521792e7107b83a97e62eb8b53e2ea
박지윤 <jiyoon0043@ajou.ac.kr>
2022-07-13 12:28:08 +0900

    Feat: create STORE_TB

---
+++ Backend/src/main/java/com/solmi/shorket/store/domain/Store.java
@@ -0,0 +1,77 @@
+package com.solmi.shorket.store.domain;
+
+import com.solmi.shorket.user.domain.LoginType;
+import com.solmi.shorket.user.domain.RoleType;
+import com.solmi.shorket.user.domain.StatusType;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "STORE_TB")
+public class Store {
+    @Id
+    @GeneratedValue
+    private Long idx;
+
+    @Column(columnDefinition = "VARCHAR(200) NOT NULL")
+    private String name;
+
+    @Column(columnDefinition = "TEXT NOT NULL")
+    private String description;
+
+    private Integer viewCount;
+
+    @Column(columnDefinition = "VARCHAR(100) NOT NULL")
+    private String si;
+
+    @Column(length = 100)
+    private String gun;
+
+    @Column(columnDefinition = "VARCHAR(100) NOT NULL")
+    private String gu;
+
+    @Column(length = 1000)
+    private String address;
+
+    @Temporal(value = TemporalType.DATE)
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Temporal(value = TemporalType.DATE)
+    @Column(nullable = false)
+    private Date endDate;
+
+    @Temporal(value = TemporalType.TIME)
+    @Column(nullable = false)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIME)
+    @Column(nullable = false)
+    private Date endTime;
+
+    private Float latitude;
+
+    private Float longitude;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+            updatable = false,
+            nullable = false)
+    private Date createdAt;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL",
+            nullable = false)
+    private Date updatedAt;
+
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'Y'")
+    @Column(nullable = false)
+    private StoreStatusType status;
+}
---
+++ Backend/src/main/java/com/solmi/shorket/store/domain/StoreStatusType.java
@@ -0,0 +1,5 @@
+package com.solmi.shorket.store.domain;
+
+public enum StoreStatusType {
+    Y, C, W; // Y: 운영중, C: 운영완료, W: 운영예정
+}
