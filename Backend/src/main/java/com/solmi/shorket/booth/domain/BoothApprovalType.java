2496605326c63567e472e1a9e7177fab77871d5c
박지윤 <jiyoon0043@ajou.ac.kr>
2022-07-13 13:15:50 +0900

    Feat: create BOOTH_TB

---
+++ Backend/src/main/java/com/solmi/shorket/booth/domain/BoothApprovalType.java
@@ -0,0 +1,5 @@
+package com.solmi.shorket.booth.domain;
+
+public enum BoothApprovalType {
+    Y, N; // Y: 승인, N: 미승인
+}
---
+++ Backend/src/main/java/com/solmi/shorket/booth/domain/BoothStatusType.java
@@ -0,0 +1,5 @@
+package com.solmi.shorket.booth.domain;
+
+public enum BoothStatusType {
+    Y, C, W; // Y: 운영중, C: 운영완료, W: 운영예정
+}
