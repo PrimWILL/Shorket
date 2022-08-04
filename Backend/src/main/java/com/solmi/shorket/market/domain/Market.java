bcaeedfeed8dbaa3bac9e82eb69919f4700575ec
Wo-ogie <siwall0105@gmail.com>
2022-08-05 01:36:05 +0900

    Feat: Implement feature `MarketInterest`

--- Backend/src/main/java/com/solmi/shorket/market/domain/Market.java
+++ Backend/src/main/java/com/solmi/shorket/market/domain/Market.java
@@ -1,12 +1,16 @@
 package com.solmi.shorket.market.domain;
 
 import com.solmi.shorket.global.BaseTimeEntity;
+import com.solmi.shorket.user.domain.MarketInterest;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Entity
@@ -44,10 +48,33 @@ public class Market extends BaseTimeEntity {
 
     private Float longitude;
 
-    @NotNull
-    @Enumerated(value = EnumType.STRING)
-    @ColumnDefault("'Y'")
-    private MarketStatusType status;    // Y: 운영중, C: 운영완료, W: 운영예정
+    @OneToMany(mappedBy = "market")
+    private Set<MarketInterest> interests = new HashSet<>();
+
+//    @NotNull
+//    @Enumerated(value = EnumType.STRING)
+//    @ColumnDefault("'W'")
+//    private MarketStatusType status;    // Y: 운영중, C: 운영완료, W: 운영예정
+
+    //== 생성 메서드 ==//
+    public static Market createMarket(String name, String description, Address address, LocalDateTime startDate, LocalDateTime endDate) {
+        return new Market(name, description, 0, address, startDate, endDate);
+    }
+
+    //== 수정 메서드==//
+    public void addViewCount() {
+        this.viewCount++;
+    }
+
+    //== Constructor ==//
+    private Market(String name, String description, Integer viewCount, Address address, LocalDateTime startDate, LocalDateTime endDate) {
+        this.name = name;
+        this.description = description;
+        this.viewCount = viewCount;
+        this.address = address;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
     //== Setter ==//
     private void setName(String name) {
--- Backend/src/main/java/com/solmi/shorket/user/domain/MarketInterest.java
+++ Backend/src/main/java/com/solmi/shorket/user/domain/MarketInterest.java
@@ -1,32 +1,56 @@
 package com.solmi.shorket.user.domain;
 
 import com.solmi.shorket.market.domain.Market;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
-@Getter
-@Setter
+import static javax.persistence.FetchType.*;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "MARKET_INTEREST_TB")
 public class MarketInterest {
+
     @Id
     @GeneratedValue
     private Integer idx;
 
-    @ManyToOne
-    @JoinColumn(name = "Market_TB_IDX", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "market_tb_idx", nullable = false)
     private Market market;
 
-    @ManyToOne
-    @JoinColumn(name = "USERS_TB_IDX", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "users_tb_idx", nullable = false)
     private User user;
 
-    @Temporal(value = TemporalType.TIMESTAMP)
-    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
-            updatable = false,
-            nullable = false)
-    private Date createdAt;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    //== 생성 메서드 ==//
+    public static MarketInterest createMarketInterest(User user, Market market) {
+        MarketInterest marketInterest = new MarketInterest();
+        marketInterest.setUser(user);
+        marketInterest.setMarket(market);
+        return marketInterest;
+    }
+
+    //== 연관관계 메서드 ==//
+    private void setMarket(Market market) {
+        this.market = market;
+        market.getInterests().add(this);
+    }
+
+    //== Setter ==//
+    private void setUser(User user) {
+        this.user = user;
+    }
 }
