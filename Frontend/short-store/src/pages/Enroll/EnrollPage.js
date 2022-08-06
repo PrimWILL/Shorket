76c4b67f9f8bbffd4b0a10a7383fe41e464c40e3
Tae-yoon Kim <chokokty0816@gmail.com>
2022-08-06 23:40:05 +0900

    Fix: fix some bugs

--- Frontend/short-store/src/pages/Enroll/EnrollPage.js
+++ Frontend/short-store/src/pages/Enroll/EnrollPage.js
@@ -1,13 +1,13 @@
 import styles from "./EnrollPage.module.css";
-import BoothEnroll from "pages/BoothEnroll";
-import AddProduct from "pages/AddProduct";
-import Signup from "pages/Signup";
+// import BoothEnroll from "pages/BoothEnroll";
+// import AddProduct from "pages/AddProduct";
+// import Signup from "pages/Signup";
 
 function EnrollPage() {
   return (
     <div className={styles.area}>
       <h1>등록페이지 입니다</h1>
-      <BoothEnroll />
+      {/* <BoothEnroll /> */}
     </div>
   );
 }
